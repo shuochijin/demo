6aad3884a64075574773864ea02fa9d8a39bcbef
shuochijin <835391854@163.com>
2019-07-08 16:19:02 +0800

    Create Demo.java

---
+++ webapp/src/main/java/controller/Demo.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="demo", urlPatterns="/demo")
+public class Demo extends HttpServlet {
+	
+	private static final long serialVersionUID = -7216740875691485716L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		OutputStream os = resp.getOutputStream();String ip = req.getHeader("X-Forwarded-For");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("Proxy-Client-IP");
+			}
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("WL-Proxy-Client-IP");
+			}
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("HTTP_CLIENT_IP");
+			}
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("HTTP_X_FORWARDED_FOR");
+			}
+			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getRemoteAddr();
+			}
+			System.out.println(ip);
+		System.out.println(resp.getCharacterEncoding());
+		os.write(new String("°¢ÈøµÂ2").getBytes());
+		os.flush();
+	}
+	
+}
